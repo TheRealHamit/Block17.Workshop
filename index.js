3bb5e48821965eb2f6c7bff77e86b02713546002
Spencer Leyh <spencerleyh@gmail.com>
2023-10-09 19:31:50 -0700

    Finished workshop functions.

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-//Prompt the user for a list of integers separated by commas
-const str = window.prompt('enter some numbers, like this', '1,2,3,3,5,9');
-
-//create an instance of numbers
-const n1 = new Numbers(str);
-console.log(n1.count());//returns count of numbers
-n1.printNumbers();//prints the number along with their indexes
-console.log(n1.odds());//returns odd numbers
-console.log(n1.evens());//returns even numbers
-console.log(n1.sum());//returns sum of numbers
-console.log(n1.product());//returns product of numbers
-console.log(n1.greaterThan(3));//returns numbers greater than another number
-console.log(n1.howMany(3));//return the count of a specific number
-
-
 // Complete the Numbers class below
 // the constructor has already been provided
 class Numbers{
@@ -27,27 +12,52 @@ class Numbers{
   }
   count(){
     //return the count of numbers in data
+    return this.data.length;
   }
   printNumbers(){
     //print the numbers in data
+    this.data.forEach(x => console.log(x));
   }
   odds(){
     //return the odd numbers in data
+    return this.data.filter(x => x%2 != 0);
   }
   evens(){
     //return the even numbers in data
+    return this.data.filter(x => x%2 == 0);
   }
   sum(){
     //return the sum of the numbers
+    return this.data.reduce((prev, curr) => {
+      return prev + curr;
+    })
   }
   product(){
     //return the product of the numbers
+    return this.data.reduce((prev, curr) => {
+      return prev * curr;
+    })
   }
   greaterThan(target){
     //return the numbers greater than the target
+    return this.data.filter(x => x > target);
   }
   howMany(target){
     //return the count of a given number
+    return this.data.filter(x => x == target).length;
   }
 }
 
+//Prompt the user for a list of integers separated by commas
+const str = window.prompt('enter some numbers, like this', '1,2,3,3,5,9');
+
+//create an instance of numbers
+const n1 = new Numbers(str);
+console.log(n1.count());//returns count of numbers
+n1.printNumbers();//prints the number along with their indexes
+console.log(n1.odds());//returns odd numbers
+console.log(n1.evens());//returns even numbers
+console.log(n1.sum());//returns sum of numbers
+console.log(n1.product());//returns product of numbers
+console.log(n1.greaterThan(3));//returns numbers greater than another number
+console.log(n1.howMany(3));//return the count of a specific number
